0d2c7094fad77fb03821b78a9495c85c750adae4
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-08-01 18:11:50 +0200

    Add sklearn in requirements.txt

--- main.py
+++ main.py
@@ -120,9 +120,9 @@ elif app_mode == "Crop Recommendation":
     st.subheader("Enter the following details to get crop recommendation:")
 
     # Input fields for user data
-    N = st.number_input("Nitrogen (N)", min_value=0.0)
-    P = st.number_input("Phosphorus (P)", min_value=0.0)
-    K = st.number_input("Potassium (K)", min_value=0.0)
+    N = st.number_input("Nitrogen (N)", min_value=0)
+    P = st.number_input("Phosphorus (P)", min_value=0)
+    K = st.number_input("Potassium (K)", min_value=0)
     temperature = st.number_input("Temperature", min_value=0.0)
     humidity = st.number_input("Humidity", min_value=0.0)
     ph = st.number_input("pH", min_value=0.0)
